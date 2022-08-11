7bcba24b0ec2d95b5a174f8cfdd4fa69af83eb5a
Yasin <107244487+masora2@users.noreply.github.com>
2022-08-12 00:42:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Insanlar insanlar = new Insanlar("Ayşe","Kara",2516);
+
+            //insanlar.İsim = "Ayşe";
+            //insanlar.Soyisim = "Yılmaz";
+            //insanlar.Yas = 15;
+            insanlar.Ekle();
+
+            
+        }
+    }
+
+
+    class Insanlar
+    {
+        public string İsim;
+        public string Soyisim;
+        public int Yas;
+
+      
+        public Insanlar(string isim,string soyisim,int yas)
+        {
+            this.İsim = isim;
+            this.Soyisim = soyisim;
+            this.Yas = yas;
+            
+        }
+
+        public Insanlar(){}
+        public void Ekle()
+        {
+            Console.WriteLine("İsim: {0}", İsim);
+            Console.WriteLine("Soyismi {0}", Soyisim);
+            Console.WriteLine("Numarası: {0}", Yas);
+        }
+
+    }
+
+ 
+}
